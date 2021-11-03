2a381e4b9f4f88bd88481bffc20f6a71036c1c43
NikolaySuhih <nikolay.suhih@nutnet.ru>
2021-11-03 15:29:47 +0400

    Condition by name

--- src/GildedRose.php
+++ src/GildedRose.php
@@ -53,8 +53,8 @@ final class GildedRose
                     }
                 } elseif ($item->name == 'Backstage passes to a TAFKAL80ETC concert') {
                     $item->quality -= $item->quality;
-                } elseif ($item->quality > 0) {
-                    if ($item->name != 'Sulfuras, Hand of Ragnaros') {
+                } elseif ($item->name != 'Sulfuras, Hand of Ragnaros') {
+                    if ($item->quality > 0) {
                         $item->quality -= 1;
                     }
                 }
