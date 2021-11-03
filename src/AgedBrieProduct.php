41a6fd48c5cfc6c6e44f22284eeff8be8c46b07f
NikolaySuhih <nikolay.suhih@nutnet.ru>
2021-11-03 13:50:19 +0400

    Clear waste :fire:

--- src/AgedBrieProduct.php
+++ src/AgedBrieProduct.php
@@ -6,11 +6,6 @@ namespace GildedRose;
 
 class AgedBrieProduct extends BaseProduct
 {
-    /** @var int Значение срока хранения после которого товар начинает терять качество быстрее */
-    const SELL_IN_TO_QTY = 0;
-    /** @var int Минимальное значение качества товара */
-    const MIN_QUALITY = 0;
-
     public function decreaseQuality($pts): BaseProduct
     {
         return parent::decreaseQuality($pts * $this->old);
