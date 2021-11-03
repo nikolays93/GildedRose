577d75bee811ad51e65565fb58cf94b462ac763c
NikolaySuhih <nikolay.suhih@nutnet.ru>
2021-11-03 14:16:13 +0400

    Reduction

--- src/GildedRose.php
+++ src/GildedRose.php
@@ -21,43 +21,43 @@ final class GildedRose
         foreach ($this->items as $item) {
             if ($item->name == 'Aged Brie' && $item->name == 'Backstage passes to a TAFKAL80ETC concert') {
                 if ($item->quality < 50) {
-                    $item->quality = $item->quality + 1;
+                    $item->quality += 1;
 
                     if ($item->name == 'Backstage passes to a TAFKAL80ETC concert') {
                         if ($item->sell_in < 11) {
-                            $item->quality = $item->quality + 1;
+                            $item->quality += 1;
                         }
 
                         if ($item->sell_in < 6) {
-                            $item->quality = $item->quality + 1;
+                            $item->quality += 1;
                         }
                     }
                 }
             } else {
                 if ($item->quality > 0) {
                     if ($item->name != 'Sulfuras, Hand of Ragnaros') {
-                        $item->quality = $item->quality - 1;
+                        $item->quality -= 1;
                     }
                 }
             }
 
             if ($item->name != 'Sulfuras, Hand of Ragnaros') {
-                $item->sell_in = $item->sell_in - 1;
+                $item->sell_in -= 1;
             }
 
             if ($item->sell_in < 0) {
                 if ($item->name == 'Aged Brie') {
                     if ($item->quality < 50) {
-                        $item->quality = $item->quality + 1;
+                        $item->quality += 1;
                     }
                 } elseif ($item->name != 'Backstage passes to a TAFKAL80ETC concert') {
                     if ($item->quality > 0) {
                         if ($item->name != 'Sulfuras, Hand of Ragnaros') {
-                            $item->quality = $item->quality - 1;
+                            $item->quality -= 1;
                         }
                     }
                 } else {
-                    $item->quality = $item->quality - $item->quality;
+                    $item->quality -= $item->quality;
                 }
             }
         }
